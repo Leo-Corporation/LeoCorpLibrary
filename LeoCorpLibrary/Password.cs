ae6bfbce6df27a5f687dda11d43bba0dacf88f78
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-03-08 15:18:29 +0100

    Initial commit

---
+++ LeoCorpLibrary/Password.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary
+{
+    public class Password
+    {
+        public string Generate(int lenght, string chars, string separator)
+        {
+            string[] usableChars = { };
+            if (chars.Contains(separator))
+            {
+                usableChars = chars.Split(new string[] { separator }, StringSplitOptions.None);
+            }
+            else
+            {
+                throw new Exception("The parameter 'chars' does not contain a sperator.");
+            }
+            string finalPassword = "";
+            Random random = new Random();
+            int number = 0;
+            if (lenght > 0)
+            {
+                for (int i = 1; i < lenght; i++)
+                {
+                    number = random.Next(0, usableChars.Length);
+                    finalPassword = finalPassword + usableChars[number];
+                }
+            }
+            else
+            {
+                throw new Exception("The parameter 'lenght' must be higher than 0.");
+            }
+            return finalPassword;
+        }
+    }
+}
