338d8b4a4f3b93a0f41e572a428a87c00cc63b6a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-03-17 09:39:22 +0100

    Ajout de fonctions liées au système de recherche de mises à jour.

---
+++ LeoCorpLibrary/Update.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary
+{
+    public class Update
+    {
+        public bool IsAvailable(string version, string lastVersion)
+        {
+            bool res;
+            if (version == lastVersion) // Sila version du logiciel = a la dernière version, alors le logiciel est à jour
+            {
+                res = false; // False : Aucune MAJs Disonibles
+            }
+            else
+            {
+                res = true; // True : MAJs Disponibles
+            }
+            return res; // Retourne la valeur finale
+        }
+        public string GetLastVersion(string lastVersionFileLink)
+        {
+            string lastVersion = "";
+            if (!string.IsNullOrWhiteSpace(lastVersionFileLink)) // Vérification que la valeur n'est pas 'null' ou remplie d'espaces blancs.
+            {
+                try
+                {
+                    WebClient webClient = new WebClient(); // Création d'un nouveau WebClient
+                    lastVersion = webClient.DownloadString(lastVersionFileLink); // Télécharge le texte de la page spécifiée et l'assigne à 'lastVersion'.
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message); // Affiche l'erreur qui s'est produite
+                }
+            }
+            else
+            {
+                throw new Exception("The parameter 'lastVersionFileLink' (string) is empty or contain only white spaces.");
+            }
+            return lastVersion;
+        }
+    }
+}
