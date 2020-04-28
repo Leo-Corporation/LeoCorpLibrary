50edc938cef5793bb7725b254c32fd49c5e79851
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-04-28 14:11:19 +0200

    Ajout de Maths.cs

---
+++ LeoCorpLibrary/Maths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary
+{
+    public class Maths
+    {
+        public double Sum(params double[] args)
+        {
+            double finalResult = 0;
+            foreach (double arg in args)
+            {
+                finalResult += arg;
+            }
+            return finalResult;
+        }
+
+        public double GetCircleArea(double radius)
+        {
+            return Math.PI * Math.Sqrt(radius);
+        }
+
+        public double GetCirclePerimeter(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
