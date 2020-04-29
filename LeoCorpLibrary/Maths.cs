8059071c7aafe651151a9ddd7811a51b644ed47f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-04-29 12:16:20 +0200

    Ajout de classes dans la classe "Maths"

--- LeoCorpLibrary/Maths.cs
+++ LeoCorpLibrary/Maths.cs
@@ -10,22 +10,22 @@ namespace LeoCorpLibrary
 {
     public class Maths
     {
-        public double Sum(params double[] args)
+        public double Sum(params double[] args) // Somme
         {
             double finalResult = 0;
             foreach (double arg in args)
             {
-                finalResult += arg;
+                finalResult += arg; // Ajoute les nombres
             }
             return finalResult;
         }
 
-        public double GetBiggestNumber(params double[] numbers)
+        public double GetBiggestNumber(params double[] numbers) // Obtient le nombre le plus grand
         {
             double biggestNumber = 0;
-            foreach (double number in numbers)
+            foreach (double number in numbers) // Pour chaque nombre
             {
-                if (number > biggestNumber)
+                if (number > biggestNumber) // Si le nombre est plus grand le plus grand nombre, alors il devient le plus grand nombre
                 {
                     biggestNumber = number;
                 }
@@ -33,64 +33,70 @@ namespace LeoCorpLibrary
             return biggestNumber;
         }
 
-        public double GetCircleArea(double radius)
+        public class Circle
         {
-            return Math.PI * Math.Sqrt(radius);
-        }
-
-        public double GetCirclePerimeter(double radius)
-        {
-            return 2 * Math.PI * radius;
-        }
-
-        public double GetTriangleArea(double height, double weight)
-        {
-            return height * weight / 2;
-        }
-
-        public double GetTrianglePerimeter(double a, double b, double c)
-        {
-            return Sum(a, b, c);
-        }
-
-        public bool IsTriangleBuildable(double side1, double side2, double side3)
-        {
-            bool result = false;
-            double biggestSide = GetBiggestNumber(side1, side2, side3);
-            if (biggestSide == side1)
+            public double GetCircleArea(double radius) // Obtenir l'aire d'un cercle
             {
-                if (biggestSide < (side2 + side3))
-                {
-                    result = true;
-                }
-                else
-                {
-                    result = false;
-                }
+                return Math.PI * Math.Sqrt(radius); // Formule
             }
-            else if (biggestSide == side2)
+
+            public double GetCirclePerimeter(double radius) // Obtenir le périmètre d'un cercle
             {
-                if (biggestSide < (side1 + side3))
-                {
-                    result = true;
-                }
-                else
-                {
-                    result = false;
-                }
+                return 2 * Math.PI * radius; // Formule
             }
-            else if (biggestSide == side3)
+        }
+
+        public class Triangle
+        {
+            public double GetTriangleArea(double height, double weight) // Obtenir l'aire d'un triangle
             {
-                if (biggestSide < (side1 + side2))
-                {
-                    result = true;
-                }
-                else
-                {
-                    result = false;
-                }
+                return height * weight / 2; // Formule
             }
-            return result;
+
+            public double GetTrianglePerimeter(double a, double b, double c) // Obtenir le périmètre d'un triangle
+            {
+                return new Maths().Sum(a, b, c); // Formule
+            }
+
+            public bool IsTriangleBuildable(double side1, double side2, double side3) // Triangle construisable ?
+            {
+                bool result = false; // Résultat final
+                double biggestSide = new Maths().GetBiggestNumber(side1, side2, side3); // Obtient le grand côté
+                if (biggestSide == side1) // Si le grand coté est side1
+                {
+                    if (biggestSide < (side2 + side3)) // Formule pour savoir si le triangle est construisable
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+                else if (biggestSide == side2) // Si le grand coté est side2
+                {
+                    if (biggestSide < (side1 + side3)) // Formule pour savoir si le triangle est construisable
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+                else if (biggestSide == side3) // Si le grand coté est side3
+                {
+                    if (biggestSide < (side1 + side2)) // Formule pour savoir si le triangle est construisable
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+                return result;
+            }
         }
     }
 }
