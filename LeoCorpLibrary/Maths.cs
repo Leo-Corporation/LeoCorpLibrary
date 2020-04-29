f235d0d0115b517c474b2ea113cc23d330205f2c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-04-29 12:01:06 +0200

    Ajout d'une fonction pour savoir si un triangle est construisable

--- LeoCorpLibrary/Maths.cs
+++ LeoCorpLibrary/Maths.cs
@@ -20,6 +20,19 @@ namespace LeoCorpLibrary
             return finalResult;
         }
 
+        public double GetBiggestNumber(params double[] numbers)
+        {
+            double biggestNumber = 0;
+            foreach (double number in numbers)
+            {
+                if (number > biggestNumber)
+                {
+                    biggestNumber = number;
+                }
+            }
+            return biggestNumber;
+        }
+
         public double GetCircleArea(double radius)
         {
             return Math.PI * Math.Sqrt(radius);
@@ -38,6 +51,46 @@ namespace LeoCorpLibrary
         public double GetTrianglePerimeter(double a, double b, double c)
         {
             return Sum(a, b, c);
+        }
+
+        public bool IsTriangleBuildable(double side1, double side2, double side3)
+        {
+            bool result = false;
+            double biggestSide = GetBiggestNumber(side1, side2, side3);
+            if (biggestSide == side1)
+            {
+                if (biggestSide < (side2 + side3))
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            else if (biggestSide == side2)
+            {
+                if (biggestSide < (side1 + side3))
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            else if (biggestSide == side3)
+            {
+                if (biggestSide < (side1 + side2))
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            return result;
         }
     }
 }
