1f9df477dac6c4c3b179b15e28b4f02d6747b53c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-05-25 12:11:45 +0200

    Ajout d'une fonction pour convertir une couleur RGB en HEX (#39)

--- LeoCorpLibrary/ColorConverter.cs
+++ LeoCorpLibrary/ColorConverter.cs
@@ -35,6 +35,27 @@ namespace LeoCorpLibrary
 			return HSVColor.FromRGB(color);
         }
 
+		/// <summary>
+		/// Permet de convertir une couleur RGB en couleur HEX (<see cref="HEXColor"/>).
+		/// </summary>
+		/// <param name="red">Rouge.</param>
+		/// <param name="green">Vert.</param>
+		/// <param name="blue">Bleu.</param>
+		/// <returns>Valeur <see cref="HEXColor"/>.</returns>
+		public static HEXColor RGBtoHEX(int red, int green, int blue)
+        {
+			return HEXColor.FromRGB(red, green, blue);
+        }
+
+		/// <summary>
+		/// Permet de convertir une couleur RGB (<see cref="Color"/>) en couleur HEX (<see cref="HEXColor"/>).
+		/// </summary>
+		/// <param name="color"></param>
+		/// <returns>Valeur <see cref="HEXColor"/>.</returns>
+		public static HEXColor RGBtoHEX(Color color)
+        {
+			return HEXColor.FromRGB(color);
+        }
     }
 
 	/// <summary>
@@ -89,6 +110,10 @@ namespace LeoCorpLibrary
 		/// <returns></returns>
 		public static HSVColor FromRGB(int red, int green, int blue)
         {
+			if (!(red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255)) // Si pas valide
+			{
+				throw new Exception("The specified argmuents aren't RGB valid."); // Erreur
+			}
 			HSVColor toReturn = new HSVColor();
 			Color color = Color.FromArgb(red, green, blue);
 			int max = Math.Max(color.R, Math.Max(color.G, color.B));
@@ -108,6 +133,40 @@ namespace LeoCorpLibrary
 	/// </summary>
     public struct HEXColor
     {
-		
+
+		/// <summary>
+		/// Valeur de la couleur.
+		/// </summary>
+		public string Value;
+
+		/// <summary>
+		/// Permet de créer une couleur HEX (<see cref="HEXColor"/>) à partir d'une couleur (<see cref="Color"/>)
+		/// </summary>
+		/// <param name="color">Couleur (<see cref="Color"/>).</param>
+		/// <returns>Valeur <see cref="HEXColor"/>.</returns>
+		public static HEXColor FromRGB(Color color)
+        {
+			HEXColor toReturn = new HEXColor();
+			toReturn.Value = ColorTranslator.FromHtml(String.Format("#{0:X2}{1:X2}{2:X2}", color.R, color.G, color.B)).Name.Remove(0, 2);
+			return toReturn;
+        }
+
+		/// <summary>
+		/// Permet de créer une couleur HEX (<see cref="HEXColor"/>) à partir de valeurs <see cref="int"/>.
+		/// </summary>
+		/// <param name="red">Rouge.</param>
+		/// <param name="blue">Bleu.</param>
+		/// <param name="green">Vert</param>
+		/// <returns>Valeur <see cref="HEXColor"/></returns>
+		public static HEXColor FromRGB(int red, int green, int blue)
+		{
+			if (!(red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255)) // Si pas valide
+            {
+				throw new Exception("The specified argmuents aren't RGB valid."); // Erreur
+            }
+			HEXColor toReturn = new HEXColor(); // Couleur HEX
+			toReturn.Value = ColorTranslator.FromHtml(String.Format("#{0:X2}{1:X2}{2:X2}", red, green, blue)).Name.Remove(0, 2); // Conversion en HEX
+			return toReturn; // Retourne la couleur HEX
+        }
     }
 }
