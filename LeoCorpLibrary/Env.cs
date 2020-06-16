9c01201e40533307c84da13ab387039a102780e8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-06-16 13:51:46 +0200

    Ajouter la possibilité de compter le nombre de fichiers présents (#47)

---
+++ LeoCorpLibrary/Env.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary
+{
+    /// <summary>
+    /// Classe regroupant des méthodes sur l'environnement de l'utilisateur.
+    /// </summary>
+    public static class Env
+    {
+        public static int GetFilesCount(string directory)
+        {
+            return Directory.GetFiles(directory, "*", SearchOption.TopDirectoryOnly).Length;
+        }
+    }
+}
