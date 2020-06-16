dbfc289b85eeea78221e630fc453d6b55c60d216
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-06-16 14:00:42 +0200

    Mise à jour de la documentation XML

--- LeoCorpLibrary/Env.cs
+++ LeoCorpLibrary/Env.cs
@@ -16,6 +16,7 @@ namespace LeoCorpLibrary
         /// Obtient le nombre de fichiers dans un répertoire sans inclure les sous-répertoires.
         /// </summary>
         /// <param name="directory">Chemin du répertoire</param>
+        /// <exception cref="DirectoryNotFoundException"></exception>
         /// <returns>Valeur de type <see cref="int"/>.</returns>
         public static int GetFilesCount(string directory)
         {
@@ -35,6 +36,7 @@ namespace LeoCorpLibrary
         /// Obtient le nombre de répertoires dans un répertoire sans inclure les sous-répertoires.
         /// </summary>
         /// <param name="directory">Chemin du répertoire.</param>
+        /// /// <exception cref="DirectoryNotFoundException"></exception>
         /// <returns>Valeur de type <see cref="int"/>.</returns>
         public static int GetDirectoryCount(string directory)
         {
