b8243daf205c52b0f73498052cbe2b62301989d3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-06-30 14:54:37 +0200

    Revert "Documentation XML '#64)"
    This reverts commit fe209e6e92378acf433d9923e44697e50bc84b0c.

--- LeoCorpLibrary/Load.cs
+++ LeoCorpLibrary/Load.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace LeoCorpLibrary
 {
     public static class Load
     {
-        public static void ListViewContent(ListView listView, string filePath)
-        {
 
-        }
     }
 }
