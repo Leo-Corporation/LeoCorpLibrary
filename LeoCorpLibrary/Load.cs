fe209e6e92378acf433d9923e44697e50bc84b0c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-06-30 14:54:24 +0200

    Documentation XML '#64)

--- LeoCorpLibrary/Load.cs
+++ LeoCorpLibrary/Load.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LeoCorpLibrary
 {
     public static class Load
     {
+        public static void ListViewContent(ListView listView, string filePath)
+        {
 
+        }
     }
 }
