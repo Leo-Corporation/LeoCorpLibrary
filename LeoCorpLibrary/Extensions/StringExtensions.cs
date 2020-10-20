9ed5b4b2e577a6192288279c7ed2e343c65b8449
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-10-20 15:09:38 +0200

    Added a variation of the CountWord() method (#142)

--- LeoCorpLibrary/Extensions/StringExtensions.cs
+++ LeoCorpLibrary/Extensions/StringExtensions.cs
@@ -16,14 +16,31 @@ namespace LeoCorpLibrary.Extensions
         /// </summary>
         /// <param name="value">The <see cref="string"/> where the words should be counted.</param>
         /// <returns>A <see cref="int"/> value.</returns>
-        public static int CountWord(this string value)
+        public static int CountWords(this string value)
         {
-            if (value.Length <= 0)
+            if (value.Length <= 0) // If the lenght of the string is less or equal to 0
             {
-                throw new ArgumentException("The 'value' argument cannot be null or empty");
+                throw new ArgumentException("The 'value' argument cannot be null or empty"); // Error
             }
 
             string[] words = value.Split(new string[] { " ", ",", ";", ".", ":", "!", "?" }, StringSplitOptions.RemoveEmptyEntries); // Get all the words
+            return words.Length; // Number of words
+        }
+
+        /// <summary>
+        /// Counts the number of words in a <see cref="string"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="string"/> where the words should be counted.</param>
+        /// <param name="wordSeparator">The separator of the words. Example: ", ; : ! ? .  ".</param>
+        /// <returns>A <see cref="int"/> value.</returns>
+        public static int CountWords(this string value, string[] wordSeparator)
+        {
+            if (value.Length <= 0) // If the lenght of the string is less or equal to 0
+            {
+                throw new ArgumentException("The 'value' argument cannot be null or empty"); // Error
+            }
+
+            string[] words = value.Split(wordSeparator, StringSplitOptions.RemoveEmptyEntries); // Get all the words
             return words.Length; // Number of words
         }
     }
