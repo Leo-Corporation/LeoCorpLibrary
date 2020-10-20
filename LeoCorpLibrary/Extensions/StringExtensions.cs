e159755c1b01c30e610f8040808ecbe1dfc8d79a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-10-20 15:01:32 +0200

    Added the possibility to count the number of words in a string (#142)

---
+++ LeoCorpLibrary/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int CountWord(this string value)
+        {
+            string[] words = value.Split(new string[] { " ", ",", ";", ".", ":", "!", "?" }, StringSplitOptions.RemoveEmptyEntries); // Get all the words
+            return words.Length; // Number of words
+        }
+    }
+}
