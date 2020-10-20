7be5943a91af732d53fcbe45171089571c68ecac
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-10-20 14:53:56 +0200

    Added the possibility to check if the number is odd or even (#141)

---
+++ LeoCorpLibrary/Extensions/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary.Extensions
+{
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int value)
+        {
+            string str = value.ToString(); // Convert to string
+
+            if (str.EndsWith("0") || str.EndsWith("2") || str.EndsWith("4") || str.EndsWith("6") || str.EndsWith("8")) // Check if the latest number is even
+            {
+                return true; // The number is even
+            }
+            else
+            {
+                return false; // The number is odd
+            }
+        }
+    }
+}
