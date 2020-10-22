500827efca788e23f3b60dde9ce76c9640c79676
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-10-22 11:36:41 +0200

    Add a varation of the Append() method (#144)

--- LeoCorpLibrary/Extensions/ArrayExtensions.cs
+++ LeoCorpLibrary/Extensions/ArrayExtensions.cs
@@ -10,8 +10,37 @@ namespace LeoCorpLibrary.Extensions
     {
         public static T[] Append<T>(this T[] array, T item)
         {
-            T[] items = { item };
-            return new List<T>(array.Concat(items)).ToArray();
+            if (array.Length == 0) // If the array is empty
+            {
+                return new T[] { item }; // Return a new array with the item
+            }
+
+            T[] items = { item }; // Create an array with the items
+            return new List<T>(array.Concat(items)).ToArray(); // Return the final array
+        }
+
+        public static T[] Append<T>(this T[] array, params T[] items)
+        {
+            if (array.Length == 0) // If the array is empty
+            {
+                T[] finalArrayEmpty = new T[items.Length]; // Final array
+                
+                for (int i = 0; i < items.Length; i++) // For each item
+                {
+                    finalArrayEmpty[i] = items[i]; // Add each item to the array
+                }
+
+                return finalArrayEmpty; // Return final array
+            }
+
+            T[] finalArray = new T[items.Length]; // Create an array
+
+            for (int i = 0; i < items.Length; i++) // For each item
+            {
+                finalArray[i] = items[i]; // Add each item to the array
+            }
+
+            return new List<T>(array.Concat(finalArray)).ToArray(); // Return the final array
         }
     }
 }
