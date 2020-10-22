0b50b5eae852204aa4f39cd328b5b52b2d5d812a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-10-22 11:27:57 +0200

    Added the possibility to append an element to an array (#144)

---
+++ LeoCorpLibrary/Extensions/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoCorpLibrary.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Append<T>(this T[] array, T item)
+        {
+            T[] items = { item };
+            return new List<T>(array.Concat(items)).ToArray();
+        }
+    }
+}
