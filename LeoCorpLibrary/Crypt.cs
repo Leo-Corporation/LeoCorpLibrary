dd4c479b609dafa7c134aff10ce620ad2c90fda3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-11-10 11:06:25 +0100

    Added the possibility to decrypt a string (#149)

--- LeoCorpLibrary/Crypt.cs
+++ LeoCorpLibrary/Crypt.cs
@@ -55,5 +55,20 @@ namespace LeoCorpLibrary
                 }
             }
         }
+
+        public static string Decrypt(string encrypt, string key)
+        {
+            using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
+            {
+                using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider()) // Create MD5CryptoServiceProvider
+                {
+                    byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+                    tripleDESCryptoService.Key = byteHash;
+                    tripleDESCryptoService.Mode = CipherMode.ECB;
+                    byte[] data = Convert.FromBase64String(encrypt); // Decrypt
+                    return Encoding.UTF8.GetString(tripleDESCryptoService.CreateDecryptor().TransformFinalBlock(data, 0, data.Length)); // Return the decrypted string
+                }
+            }
+        }
     }
 }
