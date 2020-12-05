fc19de5a6cd189b171e22a7fddfd5fcc5e9f9630
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-12-05 10:16:08 +0100

    Upgraded to .NET 5 (#163)

--- LeoCorpLibrary/Maths.cs
+++ LeoCorpLibrary/Maths.cs
@@ -24,7 +24,6 @@ SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
