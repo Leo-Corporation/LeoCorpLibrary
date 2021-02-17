45f6c2b2ae5ae3c598621b7fc8e81f02cca7b75b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-02-17 15:51:39 +0100

    Ported LeoCorpLibrary additions to LeoCorpLibrary.Core

--- LeoCorpLibrary.Core/Crypt.cs
+++ LeoCorpLibrary.Core/Crypt.cs
@@ -23,8 +23,11 @@ SOFTWARE.
 */
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LeoCorpLibrary.Core
 {
@@ -38,9 +41,15 @@ namespace LeoCorpLibrary.Core
         /// </summary>
         /// <param name="source">The <see cref="string"/> to encrypt.</param>
         /// <param name="key">The key that will be used to encrypt and decrypt the string.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         /// <returns>A <see cref="string"/> value.</returns>
         public static string Encrypt(string source, string key)
         {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException(nameof(source), "The string cannot be null.");
+            }
+
             using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
             {
                 using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider()) // Create MD5CryptoServiceProvider
@@ -59,9 +68,15 @@ namespace LeoCorpLibrary.Core
         /// </summary>
         /// <param name="encrypt">The encrypted <see cref="string"/>.</param>
         /// <param name="key">The key that will be used to encrypt and decrypt the string.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         /// <returns>A <see cref="string"/> value.</returns>
         public static string Decrypt(string encrypt, string key)
         {
+            if (string.IsNullOrEmpty(encrypt))
+            {
+                throw new ArgumentNullException(nameof(encrypt), "The string cannot be null.");
+            }
+
             using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
             {
                 using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider()) // Create MD5CryptoServiceProvider
@@ -71,6 +86,130 @@ namespace LeoCorpLibrary.Core
                     tripleDESCryptoService.Mode = CipherMode.ECB;
                     byte[] data = Convert.FromBase64String(encrypt); // Decrypt
                     return Encoding.UTF8.GetString(tripleDESCryptoService.CreateDecryptor().TransformFinalBlock(data, 0, data.Length)); // Return the decrypted string
+                }
+            }
+        }
+
+        /// <summary>
+        /// Encrypts a <see cref="string"/> using RSA encryption.
+        /// </summary>
+        /// <param name="str">The <see cref="string"/> to encrypt.</param>
+        /// <param name="rsaParameters">The RSA Key.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>A <see cref="string"/> value.</returns>
+        public static string EncryptRSA(string str, RSAParameters rsaParameters)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException(nameof(str), "The string cannot be null.");
+            }
+
+            UnicodeEncoding unicodeEncoding = new UnicodeEncoding(); // Create a new UnicodeEncoding
+
+            byte[] encryptedData;
+            using (RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                rSACryptoServiceProvider.ImportParameters(rsaParameters);
+                encryptedData = rSACryptoServiceProvider.Encrypt(unicodeEncoding.GetBytes(str), false); // Encrypt
+            }
+            return unicodeEncoding.GetString(encryptedData); // Convert byte[] to string
+        }
+
+        /// <summary>
+        /// Decrypts a <see cref="string"/> using RSA encryption.
+        /// </summary>
+        /// <param name="encrypted">The encrypted <see cref="string"/>.</param>
+        /// <param name="rsaParameters">The RSA Key.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>A <see cref="string"/> value.</returns>
+        public static string DecryptRSA(string encrypted, RSAParameters rsaParameters)
+        {
+            if (string.IsNullOrEmpty(encrypted))
+            {
+                throw new ArgumentNullException(nameof(encrypted), "The string cannot be null.");
+            }
+
+            UnicodeEncoding unicodeEncoding = new UnicodeEncoding(); // Create a new UnicodeEncoding
+
+            byte[] decryptedData;
+            using (RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                rSACryptoServiceProvider.ImportParameters(rsaParameters);
+                decryptedData = rSACryptoServiceProvider.Decrypt(unicodeEncoding.GetBytes(encrypted), false); // Decrypt
+            }
+            return unicodeEncoding.GetString(decryptedData); // Convert byte[] to string
+        }
+
+        /// <summary>
+        /// Encrypt a <see cref="string"/> using AES ecnryption.
+        /// </summary>
+        /// <param name="str">The <see cref="string"/> to encrypt.</param>
+        /// <param name="key">The key.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>A <see cref="string"/> value.</returns>
+        public static string EncryptAES(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException(nameof(str), "The string cannot be null.");
+            }
+
+            byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            int BlockSize = 128;
+
+            byte[] bytes = Encoding.Unicode.GetBytes(str); // Convert
+
+
+            SymmetricAlgorithm crypt = Aes.Create(); // Create AES
+            HashAlgorithm hash = MD5.Create(); // Create MD5
+
+            crypt.BlockSize = BlockSize; // Define block size
+            crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(key)); // Create key
+            crypt.IV = IV; // IV
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, crypt.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(bytes, 0, bytes.Length);
+                }
+
+                return Convert.ToBase64String(memoryStream.ToArray()); // Convert and return
+            }
+        }
+
+        /// <summary>
+        /// Decrypts a <see cref="string"/> using AES ecnryption.
+        /// </summary>
+        /// <param name="encrypted">The <see cref="string"/> to decrypt.</param>
+        /// <param name="key">The key.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>A <see cref="string"/> value.</returns>
+        public static string DecryptAES(string encrypted, string key)
+        {
+            if (string.IsNullOrEmpty(encrypted))
+            {
+                throw new ArgumentNullException(nameof(encrypted), "The string cannot be null.");
+            }
+
+            byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+            byte[] bytes = Convert.FromBase64String(encrypted); // Convert string to byte[]
+
+            SymmetricAlgorithm crypt = Aes.Create(); // Create AES
+            HashAlgorithm hash = MD5.Create(); // Create MD5
+
+            crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(key)); // Key
+            crypt.IV = IV;
+
+            using (MemoryStream memoryStream = new MemoryStream(bytes))
+            {
+                using (CryptoStream cryptoStream =
+                   new CryptoStream(memoryStream, crypt.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    byte[] decryptedBytes = new byte[bytes.Length];
+                    cryptoStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+                    return Encoding.Unicode.GetString(decryptedBytes); // Return
                 }
             }
         }
