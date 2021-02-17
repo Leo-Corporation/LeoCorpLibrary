59040576ea566a768cb623aa0bf2f061307ebcf3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-02-17 15:46:07 +0100

    Added the possibility to decrypt a string with AES encryption (#185)

--- LeoCorpLibrary/Crypt.cs
+++ LeoCorpLibrary/Crypt.cs
@@ -166,6 +166,29 @@ namespace LeoCorpLibrary
             }
         }
 
-        
+        public static string DecryptAES(string encrypted, string key)
+        {
+            byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            int BlockSize = 128;
+
+            byte[] bytes = Convert.FromBase64String(encrypted); // Convert string to byte[]
+
+            SymmetricAlgorithm crypt = Aes.Create(); // Create AES
+            HashAlgorithm hash = MD5.Create(); // Create MD5
+
+            crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(key)); // Key
+            crypt.IV = IV;
+
+            using (MemoryStream memoryStream = new MemoryStream(bytes))
+            {
+                using (CryptoStream cryptoStream =
+                   new CryptoStream(memoryStream, crypt.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    byte[] decryptedBytes = new byte[bytes.Length];
+                    cryptoStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+                    return Encoding.Unicode.GetString(decryptedBytes); // Return
+                }
+            }
+        }
     }
 }
