17030570c92f47de954eb43db98f40386aec450b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-03 14:38:04 +0200

    Added the possibility to get if a triangle is a right triangle (#246)

--- LeoCorpLibrary/Maths.cs
+++ LeoCorpLibrary/Maths.cs
@@ -218,6 +218,25 @@ namespace LeoCorpLibrary
 				}
 				return result;
 			}
+
+
+			public static bool IsRight(double side1, double side2, double side3)
+			{
+				double hypotenuse = GetBiggestNumber(side1, side2, side3); // Get hypotenuse
+
+				if (hypotenuse == side1)
+				{
+					return (side1 * side1) == (side2 * side2) + (side3 * side3); // Pythagorus
+				}
+				else if (hypotenuse == side2)
+				{
+					return (side1 * side2) == (side1 * side1) + (side3 * side3); // Pythagorus
+				}
+				else
+				{
+					return (side3 * side3) == (side2 * side2) + (side1 * side1); // Pythagorus
+				}
+			}
 		}
 
 		/// <summary>
