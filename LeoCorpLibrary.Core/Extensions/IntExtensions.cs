bc81dfd70e5c71c0b6b8b46f164700c67528fc8b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-10-03 11:13:35 +0200

    Refactored the IsEven() method (#260)

--- LeoCorpLibrary.Core/Extensions/IntExtensions.cs
+++ LeoCorpLibrary.Core/Extensions/IntExtensions.cs
@@ -39,19 +39,7 @@ namespace LeoCorpLibrary.Core.Extensions
 		/// </summary>
 		/// <param name="value">The <see cref="int"/> to check.</param>
 		/// <returns>A <see cref="bool"/> value.</returns>
-		public static bool IsEven(this int value)
-		{
-			string str = value.ToString(); // Convert to string
-
-			if (str.EndsWith("0") || str.EndsWith("2") || str.EndsWith("4") || str.EndsWith("6") || str.EndsWith("8")) // Check if the latest number is even
-			{
-				return true; // The number is even
-			}
-			else
-			{
-				return false; // The number is odd
-			}
-		}
+		public static bool IsEven(this int value) => value % 2 == 0;
 
 		/// <summary>
 		/// Converts an <see cref="int"/> value to a <see cref="double"/> value.
